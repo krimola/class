88e16d72c75a1e1132a798e373f27404295b2f4b
Abdullah Karim <anthony@Abdullahs-MacBook-Pro.local>
2025-03-26 11:10:01 +0100

    This is the codes for the analysis of diamond data

---
+++ Day3 training .R
@@ -0,0 +1,23 @@
+library(tidyverse)
+#load data
+participants_data<-read.csv("participants_data.csv")
+
+
+#renam the data sett
+participants_data<-dplyr::rename(participants_data,commute=km_home_to_zef)
+
+participants_data_more_name_conversions<-rename(participants_data_rename_commute,name_lenth=letters_in_first_name)
+colnames(participants_data)
+summary_dist<-participants_data$commute
+summary_dist
+head(summary_dist)
+summary_dist<-participants_data$commute
+head(participants_data$commute)
+participants_data%>%
+  split(.$gender)%>%
+  map(~
+        lm(number_of_publications~number_of_siblings,
+           data=.))%>%
+  map(summary) %>% 
+  map_dbl("r.squared")
+        
